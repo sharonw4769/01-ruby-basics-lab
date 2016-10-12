4981ba78540850f781278726fba78c82865bbc55
sharonw4769 <sharonw4769@hstat.org>
2016-10-12 22:33:48 +0000

    finish hw

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1, num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(value)
+  return value
 end
 
-def greeting
+def greeting(x, y)
 
 end
 
-def return_a_value
-    
+def return_a_value()
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(default_pizza = "cheese")
+    default_pizza
 end
 
-def whisper
-    
+def whisper(answer)
+    return answer.downcase
 end
